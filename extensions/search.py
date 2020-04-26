d287672df52606a8057a967e82c706482f15d21c
IllusionDX <flnyrt@gmail.com>
2020-04-26 00:29:46 -0400

    Minor change on derpibooru commnand

--- extensions/search.py
+++ extensions/search.py
@@ -2,8 +2,8 @@ import discord
 import aiohttp
 import random
 import math
+import re
 
-from PIL import ImageFilter
 from discord.ext import commands
 from defs import *
 from config import filters
@@ -14,6 +14,24 @@ class Search(commands.Cog, name = "Busqueda"):
 
     @commands.command(aliases=["derpi", "derpy", "db"])
     async def derpibooru(self, ctx, *, arg:str = None):
+
+        def get_db_id(q):
+            prog = re.compile("(?<=[images/])([0-9]+)")
+            m = re.search(prog, q)
+            if m is not None:
+                return m[0]
+            else:
+                pass
+
+        m = get_db_id(arg)
+        if m:
+            arg = m
+            if ctx.me.permissions_in(ctx.channel).manage_messages:
+                await ctx.message.delete()
+            else:
+                await ctx.send("¡No tengo permisos para reemplazar tu mensaje!")
+                return
+
         if arg is None:
             query = "*"
         elif len(arg.split(",")) == 1 and arg.isdigit():
@@ -21,6 +39,7 @@ class Search(commands.Cog, name = "Busqueda"):
         else:
             query = arg
 
+                
         embed = discord.Embed(color=discord.Color.orange())
         url = "https://derpibooru.org/api/v1/json/search/images"
         params = {
@@ -60,7 +79,7 @@ class Search(commands.Cog, name = "Busqueda"):
         if not data['uploader']:
             data['uploader'] = "Anónimo"
 
-        if not data['source_url'] or data['source_url'] is None:
+        if not data['source_url']:
             data['source_url'] = "Ninguna"
 
         embed.set_author(name="Derpibooru", url=f"https://www.derpibooru.org/images/{data['id']}",icon_url="https://i.imgur.com/K311uwc.png")
