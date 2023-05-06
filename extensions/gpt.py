d3851a92d3a119a6dcd02acd0e4d12e64e0598f1
IllusionDX <flnyrt@gmail.com>
2023-05-05 22:34:00 -0400

    Testing

--- extensions/gpt.py
+++ extensions/gpt.py
@@ -1,31 +1,63 @@
 import discord
 from discord.ext import commands
 from modules import chatbot
+from queue import Queue
+import concurrent.futures
+import asyncio
 
 class GPT(commands.Cog):
-	completion = chatbot.Completion()
+    completion = chatbot.Completion()
+    queue = Queue()
+    lock = asyncio.Lock()
 
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-	@commands.group(name="chat", invoke_without_command=True, aliases=["c"])
-	async def chat(self, ctx, *, message:str = None):
-		prompt = message
-		response = ""
+    @commands.group(name="chat", invoke_without_command=True, aliases=["c"])
+    async def chat(self, ctx, *, message:str = None):
+        prompt = message
+        response = ""
 
-		async with ctx.typing():
-			response = self.completion.get_response(prompt)
+        # Add the message to the queue
+        async with self.lock:
+            self.queue.put((ctx.channel.id, ctx.author.id))
 
-		# Do something with the message ID
-		await ctx.send(f"{response}")
+        async with self.lock:
+            # Wait for the previous order to finish executing
+            while not self.queue.empty() and self.queue.queue[0] != (ctx.channel.id, ctx.author.id):
+                await asyncio.sleep(0.1)
 
-	@chat.command(name="reset")
-	async def reset(self, ctx):
-		# Reset the persistent message_id
-		self.completion.reset()
+            # Execute the current order
+            async with ctx.typing():
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    response = await self.bot.loop.run_in_executor(executor, self.completion.get_response, prompt)
 
-		# Send a message to the user
-		await ctx.send("El contexto ha sido reseteado.")
+            # Remove the current order from the queue
+            self.queue.get()
+
+        # Send the response
+        await ctx.send(response)
+
+    @chat.command(name="reset")
+    async def reset(self, ctx):
+        async with self.lock:
+            # Add the reset order to the queue
+            self.queue.put((ctx.channel.id, ctx.author.id))
+
+        async with self.lock:
+            # Wait for the previous order to finish executing
+            while not self.queue.empty() and self.queue.queue[0] != (ctx.channel.id, ctx.author.id):
+                await asyncio.sleep(0.1)
+
+            # Execute the reset order
+            self.completion.reset()
+
+            # Remove the reset order from the queue
+            self.queue.get()
+
+        async with ctx.typing():
+            # Send a message to the user
+            await ctx.send("El contexto ha sido reseteado.")
 
 async def setup(bot):
-	await bot.add_cog(GPT(bot))
+    await bot.add_cog(GPT(bot))
