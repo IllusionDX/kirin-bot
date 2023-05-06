3558044fc6b1bfa85df408c7f1fe175490624131
IllusionDX <flnyrt@gmail.com>
2023-05-06 07:38:27 -0400

    I'm done trying for now

--- extensions/gpt.py
+++ extensions/gpt.py
@@ -1,88 +1,78 @@
 import discord
 from discord.ext import commands
 from modules import chatbot
-from queue import Queue
-import concurrent.futures
 import asyncio
 
 class GPT(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.completion = chatbot.Completion()
-        self.queue = Queue()
-        self.lock = asyncio.Lock()
+	def __init__(self, bot):
+		self.bot = bot
+		self.completion = chatbot.Completion()
+		self.queue = asyncio.Queue()
+		self.lock = asyncio.Lock()
 
-    @commands.group(name="chat", invoke_without_command=True, aliases=["c"])
-    async def chat(self, ctx, *, message: str = None):
-        if not message:
-            await ctx.send("Por favor escribe algo para poder responder!")
-            return
+	@commands.group(name="chat", invoke_without_command=True, aliases=["c"])
+	async def chat(self, ctx, *, message: str = None):
+		if not message:
+			await ctx.send("Por favor escribe algo para poder responder!")
+			return
 
-        prompt = message
-        response = ""
+		prompt = message
+		response = ""
 
-        async with self.lock:
-            # Add the message to the queue
-            self.queue.put((ctx.channel.id, ctx.author.id))
+		# Add the message to the queue
+		await self.queue.put((ctx.channel.id, ctx.author.id))
 
-        async with self.lock:
-            # Wait for the previous order to finish executing
-            while not self.queue.empty() and self.queue.queue[0] != (ctx.channel.id, ctx.author.id):
-                await asyncio.sleep(0.1)
+		async with self.lock:
+			# Wait for the previous order to finish executing
+			while not self.queue.empty() and self.queue._queue[0] != (ctx.channel.id, ctx.author.id):
+				await asyncio.sleep(2)
 
-            # Execute the current order
-            async with ctx.typing():
-                with concurrent.futures.ThreadPoolExecutor() as executor:
-                    try:
-                        response = await self.bot.loop.run_in_executor(executor, self.completion.get_response, prompt)
-                    except Exception as e:
-                        print(f"An error occurred while generating response: {e}")
-                        response = "Ha ocurrido un error al generar la respuesta."
+			# Execute the current order
+			async with ctx.typing():
+				try:
+					response = await asyncio.to_thread(self.completion.get_response, prompt)
+				except Exception as e:
+					print(f"An error occurred while generating response: {e}")
+					response = "Ha ocurrido un error al generar la respuesta."
 
-            # Remove the current order from the queue
-            self.queue.get()
+			# Remove the current order from the queue
+			await self.queue.get()
 
-        # Send the response
-        if len(response) > 2000:
-            response_list = response.split()
-            response_chunks = []
-            current_chunk = ""
-            for word in response_list:
-                if len(current_chunk + " " + word) <= 2000:
-                    current_chunk += " " + word
-                else:
-                    response_chunks.append(current_chunk)
-                    current_chunk = word
-            response_chunks.append(current_chunk)
+		# Send the response
+		if len(response) > 2000:
+			# Find the last space character in the response before the limit
+			last_space_index = response[:2000].rfind(' ')
+			if last_space_index == -1:
+				# If no space is found, cut at the character limit anyway
+				last_space_index = 2000
+			response_chunks = [response[i:i+last_space_index].strip() for i in range(0, len(response), last_space_index)]
+			for chunk in response_chunks:
+				await ctx.send(chunk.strip())
+		else:
+			await ctx.send(response.strip())
 
-            for chunk in response_chunks:
-                await ctx.send(chunk.strip())
-        else:
-            await ctx.send(response.strip())
+	@chat.command(name="reset")
+	async def reset(self, ctx):
+		async with self.lock:
+			# Add the reset order to the queue
+			await self.queue.put((ctx.channel.id, ctx.author.id))
 
-    @chat.command(name="reset")
-    async def reset(self, ctx):
-        async with self.lock:
-            # Add the reset order to the queue
-            self.queue.put((ctx.channel.id, ctx.author.id))
+			# Wait for the previous order to finish executing
+			while not self.queue.empty() and self.queue._queue[0] != (ctx.channel.id, ctx.author.id):
+				await asyncio.sleep(2)
 
-        async with self.lock:
-            # Wait for the previous order to finish executing
-            while not self.queue.empty() and self.queue.queue[0] != (ctx.channel.id, ctx.author.id):
-                await asyncio.sleep(0.1)
+			# Execute the reset order
+			try:
+				self.completion.reset()
+			except Exception as e:
+				print(f"An error occurred while resetting context: {e}")
 
-            # Execute the reset order
-            try:
-                self.completion.reset()
-            except Exception as e:
-                print(f"An error occurred while resetting context: {e}")
+			# Remove the reset order from the queue
+			await self.queue.get()
 
-            # Remove the reset order from the queue
-            self.queue.get()
-
-        async with ctx.typing():
-            # Send a message to the user
-            await ctx.send("El contexto ha sido reseteado.")
+		async with ctx.typing():
+			# Send a message to the user
+			await ctx.send("El contexto ha sido reseteado.")
 
 async def setup(bot):
-    await bot.add_cog(GPT(bot))
+	await bot.add_cog(GPT(bot))
--- modules/chatbot.py
+++ modules/chatbot.py
@@ -7,93 +7,90 @@ from curl_cffi import requests
 from fake_useragent import UserAgent
 
 class Completion:
-    # experimental
-    part1 = '{"role":"assistant","id":"chatcmpl'
-    part2 = '"},"index":0,"finish_reason":null}]}}'
-    regex = rf'{part1}(.*){part2}'
+	part1 = '{"role":"assistant","id":"chatcmpl'
+	part2 = '"},"index":0,"finish_reason":null}]}}'
+	regex = rf'{part1}(.*){part2}'
 
-    timer = None
-    message_queue = Queue()
-    stream_completed = False
-    last_msg_id = None
-    thread = None
+	message_queue = Queue()
+	stream_completed = False
+	last_msg_id = None
+	thread = None
 
-    @staticmethod
-    def request(prompt: str, proxy: Optional[str] = None):
-        headers = {
-            'authority': 'chatbot.theb.ai',
-            'content-type': 'application/json',
-            'origin': 'https://chatbot.theb.ai',
-            'user-agent': UserAgent().random,
-        }
+	@staticmethod
+	def request(prompt: str, proxy: Optional[str] = None):
+		headers = {
+			'authority': 'chatbot.theb.ai',
+			'content-type': 'application/json',
+			'origin': 'https://chatbot.theb.ai',
+			'user-agent': UserAgent().edge,
+		}
 
-        proxies = {'http': 'http://' + proxy, 'https': 'http://' + proxy} if proxy else None
+		proxies = {'http': 'http://' + proxy, 'https': 'http://' + proxy} if proxy else None
 
-        options = {}
-        if Completion.last_msg_id:
-            options['parentMessageId'] = Completion.last_msg_id
+		options = {}
+		if Completion.last_msg_id:
+			options['parentMessageId'] = Completion.last_msg_id
 
-        try:
-            response = requests.post(
-                'https://chatbot.theb.ai/api/chat-process',
-                headers=headers,
-                proxies=proxies,
-                content_callback=Completion.handle_stream_response,
-                json={'prompt': prompt, 'options': options},
-                timeout=100000
-            )
+		try:
+			response = requests.post(
+				'https://chatbot.theb.ai/api/chat-process',
+				headers=headers,
+				proxies=proxies,
+				content_callback=Completion.handle_stream_response,
+				json={'prompt': prompt, 'options': options},
+				timeout=360
+			)
+			response.raise_for_status()
 
-            response.raise_for_status()
-            Completion.stream_completed = True
+		except Exception as e:
+			raise
 
-        except Exception as e:
-            print(f"An error occurred while sending the request: {e}")
-            Completion.stream_completed = True
+		Completion.stream_completed = True
 
-    @staticmethod
-    def create(prompt: str, proxy: Optional[str] = None) -> Generator[str, None, None]:
-        Completion.stream_completed = False
+	@staticmethod
+	def create(prompt: str, proxy: Optional[str] = None) -> Generator[str, None, None]:
+		Completion.stream_completed = False
 
-        Completion.thread = Thread(target=Completion.request, args=[prompt, proxy])
-        Completion.thread.start()
+		Completion.thread = Thread(target=Completion.request, args=[prompt, proxy])
+		Completion.thread.start()
 
-        while not Completion.stream_completed or not Completion.message_queue.empty():
-            try:
-                message = Completion.message_queue.get(timeout=0.01)
-                for message in findall(Completion.regex, message):
-                    message_json = loads(Completion.part1 + message + Completion.part2)
-                    Completion.last_msg_id = message_json['id']
-                    yield message_json['text']
+		while not Completion.stream_completed or not Completion.message_queue.empty():
+			try:
+				message = Completion.message_queue.get_nowait()
+				for message in findall(Completion.regex, message):
+					message_json = loads(Completion.part1 + message + Completion.part2)
+					Completion.last_msg_id = message_json['id']
+					yield message_json['text']
 
-            except Empty:
-                pass
+			except Empty:
+				pass
 
-        Completion.message_queue.queue.clear()
+		Completion.message_queue.queue.clear()
 
-    @staticmethod
-    def handle_stream_response(response):
-        Completion.message_queue.put(response.decode())
+	@staticmethod
+	def handle_stream_response(response):
+		Completion.message_queue.put(response.decode())
 
-    @staticmethod
-    def get_response(prompt: str, proxy: Optional[str] = None) -> Optional[str]:
-        response_list = []
-        try:
-            for message in Completion.create(prompt, proxy):
-                response_list.append(message)
+	@staticmethod
+	def get_response(prompt: str, proxy: Optional[str] = None) -> Optional[str]:
+		response_list = []
+		try:
+			for message in Completion.create(prompt, proxy):
+				response_list.append(message)
 
-            if response_list:
-                return response_list[-1]
+			if response_list:
+				return response_list[-1]
 
-        except Exception as e:
-            print(f"An error occurred while getting the response: {e}")
+		except Exception as e:
+			raise
 
-        return None
+		return None
 
-    @staticmethod
-    def reset():
-        Completion.stream_completed = False
-        Completion.last_msg_id = None
-        if Completion.thread is not None:
-            Completion.thread.join()
-            Completion.thread = None
-        Completion.message_queue.queue.clear()
+	@staticmethod
+	def reset():
+		Completion.stream_completed = False
+		Completion.last_msg_id = None
+		if Completion.thread is not None:
+			Completion.thread.join()
+			Completion.thread = None
+		Completion.message_queue.queue.clear()
