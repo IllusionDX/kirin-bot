1e00f4c4ce781eb1776ff3f6559a7dd64fd8d897
IllusionDX <flnyrt@gmail.com>
2023-07-13 23:01:37 -0400

    Garbage code

--- modules/chatbot.py
+++ modules/chatbot.py
@@ -1,81 +1,64 @@
 import aiohttp
 import json
+import asyncio
 
 class Completion:
-    @staticmethod
-    async def create(
-        systemMessage: str = "You are a helpful assistant",
-        prompt: str = "",
-        parentMessageId: str = "",
-        temperature: float = 0.8,
-        top_p: float = 1,
-        server_id: str = None,
-    ):
-        json_data = {
-            "prompt": prompt,
-            "options": {"parentMessageId": parentMessageId},
-            "systemMessage": systemMessage,
-            "temperature": temperature,
-            "top_p": top_p,
-        }
+	MAX_RETRIES = 3
 
-        url = "https://www.aitianhu.com/api/chat-process"
-        headers = {
-            "Accept": "application/json, text/plain, */*",
-            "Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "en-US",
-            "Connection": "keep-alive",
-            "Content-Length": str(len(json.dumps(json_data))),
-            "Content-Type": "application/json",
-            "Origin": "https://www.aitianhu.com",
-            "Referer": "https://www.aitianhu.com/",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 OPR/99.0.0.0",
-        }
+	@staticmethod
+	async def create(
+		systemMessage: str = "You are a helpful assistant",
+		prompt: str = "",
+		parentMessageId: str = "",
+		temperature: float = 0.8,
+		top_p: float = 1,
+		server_id: str = None,
+	):
+		json_data = {
+			"prompt": prompt,
+			"options": {"parentMessageId": parentMessageId},
+			"systemMessage": systemMessage,
+			"temperature": temperature,
+			"top_p": top_p,
+		}
 
-        if server_id:
-            headers["Cookie"] = f"SERVERID={server_id}"
+		url = "https://www.aitianhu.com/api/chat-process"
+		headers = {
+			"Accept": "application/json, text/plain, */*",
+			"Accept-Encoding": "gzip, deflate",
+			"Accept-Language": "en-US",
+			"Connection": "keep-alive",
+			"Content-Type": "application/json",
+			"Origin": "https://www.aitianhu.com",
+			"Referer": "https://www.aitianhu.com/",
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 OPR/99.0.0.0",
+		}
 
-        async with aiohttp.ClientSession() as session:
-            retries = 3
-            while retries > 0:
-                async with session.post(url, json=json_data, headers=headers) as response:
-                    try:
-                        if response.status != 200:
-                            raise Exception(f"HTTP error: {response.status} - {response.reason}")
+		if server_id:
+			headers["Cookie"] = f"SERVERID={server_id}"
 
-                        content = await response.text(encoding='utf-8')
+		retries = 0
+		while retries < Completion.MAX_RETRIES:
+			try:
+				async with aiohttp.ClientSession() as session:
+					async with session.post(url, json=json_data, headers=headers, raise_for_status=True) as response:
+						content = await response.text(encoding='utf-8')
+						if content:
+							server_id_cookie = response.cookies.get('SERVERID')
+							server_id_value = server_id_cookie.value if server_id_cookie else None
 
-                        if not content:
-                            retries -= 1
-                            continue
+							json_data = [item.rstrip() for item in content.split('\n')]
+							to_json = json.loads(json_data[-1])
 
-                        server_id_cookie = response.cookies.get('SERVERID')
-                        if server_id_cookie:
-                            server_id_value = server_id_cookie.value
+							return {
+								"json": to_json,
+								"cookie": server_id_value
+								}
 
-                            json_data = [item.rstrip() for item in content.split('\n')]
-                            to_json = json.loads(json_data[-1])
-                            return {
-                                "json": to_json,
-                                "cookie": server_id_value
-                            }
-                        else:
-                            json_data = [item.rstrip() for item in content.split('\n')]
-                            to_json = json.loads(json_data[-1])
-                            return {
-                                "json": to_json,
-                                "cookie": None
-                            }
-
-                    finally:
-                        response.release()
-                await session.close()
-                break
-
-            if retries == 0:
-                raise Exception("Failed to get a non-empty response after multiple retries.")
-
-        try:
-            raise Exception("No response received.")
-        except Exception as e:
-            raise Exception(f"Error parsing JSON response: {e}\nResponse: {content}") from e
+			except Exception as e:
+				if retries == 3:
+					raise e
+				else:
+					print(f"Ha ocurrido un error: {str(e)}")
+					# Retry if there is an error
+					retries += 1
